b602b03c36da4efc6feffc0124b7871626828550
BisiDavi <oludavidconnect@gmail.com>
2022-12-17 21:36:43 +0100

    icons updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
       </Head>
       <div className="container mx-auto flex items-center justify-between">
         <Logo />
-        <div className="icons">
+        <div className="icons flex">
           <Search />
           <Wishlist />
           <Person />
