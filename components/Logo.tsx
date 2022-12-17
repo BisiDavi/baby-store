f4324829d579f18590b6f45635bcbe633e922a75
BisiDavi <oludavidconnect@gmail.com>
2022-12-17 21:22:26 +0100

    logo added

---
+++ components/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="text-blue-500 font-bold text-2xl border px-4 py-1">
+      <span className="text-pink-500">Kid</span>
+      Store
+    </div>
+  );
+}
