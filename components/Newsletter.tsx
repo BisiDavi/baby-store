be6f52177a5b086ff8a248995083d7f3864bfa55
BisiDavi <oludavidconnect@gmail.com>
2022-12-17 20:34:44 +0100

    icons added

---
+++ components/Newsletter.tsx
@@ -0,0 +1,11 @@
+export default function Newsletter() {
+  return (
+    <div>
+        <div className="column-1">
+            <div className="icon"></div>
+            <h4>SIGN UP FOR NEWSLETTER</h4>
+        </div>
+        <div className="column-2"></div>
+    </div>
+  )
+}
