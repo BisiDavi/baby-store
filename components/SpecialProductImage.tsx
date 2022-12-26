607f4cd34a06f48715c7fbca5317f6303cf66d6e
BisiDavi <oludavidconnect@gmail.com>
2022-12-26 23:08:17 +0100

    product view updated

--- components/SpecialProductImage.tsx
+++ components/SpecialProductImage.tsx
@@ -18,8 +18,8 @@ export default function SpecialProductImage({
     setSelectedProduct(item);
   }
   return (
-    <div className="w-1/2 flex mr-14">
-      <ul className="selectProduct w-1/4">
+    <div className="w-1/2 flex space-x-4 mr-14">
+      <ul className="selectProduct bg-white w-1/4 space-y-6">
         {specialProductsContent.map((item) => (
           <li key={item.id}>
             <button
@@ -31,19 +31,20 @@ export default function SpecialProductImage({
                 alt={item.title}
                 height={100}
                 width={135}
+                className="h-1/2 w-1/2 mx-auto"
               />
             </button>
           </li>
         ))}
       </ul>
-      <div className="mainProduct w-3/4 relative border flex rounded-xl">
+      <div className="mainProduct items-center bg-white w-3/4 relative border flex rounded-xl">
         <Tag className="bg-blue-900 absolute top-4 z-5 left-4" />
         <Image
           src={selectedProduct.image}
           alt={selectedProduct.title}
-          height={500}
-          width={500}
-          className="rounded-xl flex mx-auto w-full"
+          height={400}
+          width={400}
+          className="rounded-xl flex mx-auto w-3/5 justify-center items-center "
         />
       </div>
     </div>
