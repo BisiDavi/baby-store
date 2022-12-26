e754331c864f190bccfcde1790af928f7c325c0d
BisiDavi <oludavidconnect@gmail.com>
2022-12-26 16:00:31 +0100

    icons added

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -3,6 +3,8 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 import blogs from "@/json/blogs.json";
 import Button from "@/components/Button";
+import Calender from "@/public/icon/Calender";
+import User from "@/public/icon/User";
 
 export default function Blog() {
   return (
@@ -29,9 +31,14 @@ export default function Blog() {
                 <div className="text w-1/2">
                   <h4 className="text-2xl font-bold mb-4">{item.title}</h4>
                   <p className="mb-2 text-gray-500">{item.text}</p>
-                  <div className="published w-3/4 flex justify-between">
-                    <div className="date"> {item.date}</div>
-                    <div className="author">{item.author}</div>
+                  <div className="published w-4/5 flex justify-between">
+                    <div className="date flex items-center">
+                      <Calender className="w-1/12 mr-2" /> {item.date}
+                    </div>
+                    <div className="author flex items-center">
+                      <User className="w-1/12 mr-2" />
+                      {item.author}
+                    </div>
                   </div>
                   <Button
                     className="rounded-xl text-white py-3 px-6 bg-blue-900 mt-14 hover:opacity-80"
