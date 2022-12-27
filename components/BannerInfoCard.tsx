9bed0b35cb7dcfc119009ef45a5652ba3aa05650
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 13:18:42 +0100

    categories updated

--- components/BannerInfoCard.tsx
+++ components/BannerInfoCard.tsx
@@ -5,19 +5,22 @@ interface Props {
     title: string;
     text: string;
     img: string;
+    className: string;
   };
 }
 
 export default function BannerInfoCard({ content }: Props) {
   return (
     <>
-      <div className={`bannerInfoCard p-8 relative h-80 rounded-xl`}>
+      <div
+        className={`bannerInfoCard p-6 lg:p-8 relative ${content.className} lg:h-80 rounded-xl`}
+      >
         <div className="content">
           <h3 className="text-xl font-bold">{content.title}</h3>
           <p className="mt-1">{content.text}</p>
           <Button
             text="Shop Now"
-            className="bg-white h-10 mt-10 rounded-lg w-32 hover:bg-gray-100"
+            className="bg-white h-10 mt-5 lg:mt-10 rounded-lg w-32 hover:bg-gray-100"
           />
         </div>
       </div>
@@ -33,6 +36,12 @@ export default function BannerInfoCard({ content }: Props) {
           .bannerInfoCard:hover {
             transform: scale(1);
             transition: transform 5s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+          }
+
+          @media (max-width: 768px) {
+            .bannerInfoCard {
+              height: unset;
+            }
           }
         `}
       </style>
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -7,12 +7,12 @@ import "@splidejs/react-splide/css";
 export default function Slider() {
   return (
     <section className="w-full">
-      <Splide options={{ autoplay: true }}>
+      <Splide options={{ autoplay: true, loop: true }}>
         {sliderContent.slider.map((item) => (
           <SplideSlide key={item.img}>
-            <div className="content p-10 lg:p-20">
+            <div className="content p-10 flex flex-col mx-auto justify-center lg:p-20">
               <h6 className="text-lg font-medium">{item.text1}</h6>
-              <h1 className="text-3xl lg:w-1/3 leading-snug font-bold my-8">
+              <h1 className="lg:text-3xl text-2xl lg:w-1/3 leading-snug font-bold my-4 lg:my-8">
                 {item.main}
               </h1>
               <p className="text-lg lg:w-1/2">{item.text2}</p>
