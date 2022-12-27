2c523931dce2a11245046ed2abc40639584e19ba
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 23:14:34 +0100

    providerlayout updated

--- layout/ProviderLayout.tsx
+++ layout/ProviderLayout.tsx
@@ -2,6 +2,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import dynamic from "next/dynamic";
 import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import store from "@/redux/store";
 
 const NextNProgress = dynamic(
   () => import(/* webpackChunkName: 'NProgress' */ "@/components/NProgress")
@@ -9,16 +14,22 @@ const NextNProgress = dynamic(
 
 export default function ProviderLayout({ children }: PropsWithChildren) {
   const queryClient = new QueryClient();
+  let persistor = persistStore(store);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextNProgress
-        color="#f54c4c"
-        height={5}
-        options={{ showSpinner: true }}
-      />
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <NextNProgress
+            color="#f54c4c"
+            height={5}
+            options={{ showSpinner: true }}
+          />
+          <ToastContainer />
+          {children}
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   );
 }
