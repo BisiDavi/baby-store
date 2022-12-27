49eba67da75b260cb1cb92b1512c7ad89221f6e2
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 13:07:53 +0100

    categories updated

--- components/SpecialProducts.tsx
+++ components/SpecialProducts.tsx
@@ -9,7 +9,7 @@ export default function SpecialProducts() {
   );
 
   return (
-    <div className="w-full my-8 px-4 lg:px-0 py-4">
+    <div className="w-full my-8 py-4">
       <h3 className="text-3xl font-bold text-center mb-4">Special Products</h3>
       <p className="text-center mb-10 text-lg lg:w-1/2 mx-auto">
         Interesting Features Is Rich And Colorful, Each Button To Bring A
--- components/TopCategories.tsx
+++ components/TopCategories.tsx
@@ -3,7 +3,9 @@ import CategoryTabView from "@/components/CategoryTabView";
 export default function TopCategories() {
   return (
     <div className="bg-white px-4 lg:px-0">
-      <h3 className="text-3xl text-center font-medium">Top Categories</h3>
+      <h3 className="text-2xl lg:text-3xl text-center font-medium">
+        Top Categories
+      </h3>
       <p className="text-center my-3 lg:w-2/3 flex mx-auto">
         Interesting Features Is Rich And Colorful, Each Button To Bring A
         Surprise To The Baby Introduction To Give Your Child Learn In Advance.
