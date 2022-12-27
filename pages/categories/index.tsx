0ebb14f4b155e1ba87630bde0c38acb8e74f349b
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 08:47:51 +0100

    categories page added

---
+++ pages/categories/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "@/layout";
+
+export default function CategoriesPage() {
+  return (
+    <Layout title="Shop from your categories at a cheap rate">index</Layout>
+  );
+}
---
+++ pages/category/[slug].tsx
@@ -0,0 +1,5 @@
+import Layout from "@/layout";
+
+export default function CategoryPage() {
+  return <Layout title="Category"></Layout>;
+}
