936d62f3ca758a5d6939ee9f3e4d28bc382f5f09
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 05:45:22 +0100

    product view updated

--- components/ProductView.tsx
+++ components/ProductView.tsx
@@ -5,26 +5,27 @@ interface Props {
   product: productType;
 }
 export default function ProductView({ product }: Props) {
+  const first4Images = product.images.slice(0, 4);
   return (
-    <div className="w-full flex space-x-4">
-      <div className="left w-1/2">
+    <div className="w-full flex-col flex space-x-4 my-8">
+      <div className="left w-full space-x-5 flex items-center">
         <Image
           src={product.thumbnail}
           alt={product.title}
           height={500}
           width={500}
-          className="w-full rounded-xl"
+          className="w-1/2 rounded-xl"
         />
-        <ul className="thumblist space-x-4 flex items-center my-4">
-          {product.images.map((image) => (
-            <li key={image}>
-              <Image src={image} alt={product.title} height={100} width={100} />
+        <ul className="thumblist grid gap-4 grid-cols-2 w-1/2">
+          {first4Images.map((image) => (
+            <li key={image} className="border p-2 rounded-lg">
+              <Image src={image} alt={product.title} height={500} width={500} />
             </li>
           ))}
         </ul>
       </div>
       <div className="right w-1/2">
-        <p>{}</p>
+        <p className="text-gray-500 font-medium">{product.brand}</p>
         <h3 className="text-xl font-bold"> {product.title}</h3>
         <div className="price flex space-x-2">
           <h5 className="text-xl font-medium">${product.price}</h5>
