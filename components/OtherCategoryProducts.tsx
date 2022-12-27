26cc49e23ddde49d905975eb6f8bce4795dba57c
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 08:34:13 +0100

    categories updated

--- components/OtherCategoryProducts.tsx
+++ components/OtherCategoryProducts.tsx
@@ -1,5 +1,7 @@
-import { fetchCategoryProducts } from "@/utils/apiRequest";
 import { useQuery } from "@tanstack/react-query";
+
+import { fetchCategoryProducts } from "@/utils/apiRequest";
+import ProductSlider from "@/components/ProductSlider";
 
 interface Props {
   category: string;
@@ -10,16 +12,29 @@ export default function OtherCategoryProducts({
   category,
   mainProductName,
 }: Props) {
-  const {} = useQuery(
+  const { data, status } = useQuery(
     ["fetch-products-in-category"],
     () => fetchCategoryProducts(category),
     {
       enabled: !!category,
     }
   );
+  const otherProducts =
+    status === "success"
+      ? data.data.products.filter((item) => item.title !== mainProductName)
+      : [];
   return (
     <div>
       <h4 className="text-center font-bold text-2xl">View more {category}</h4>
+      <div className="other-products mt-4">
+        {status === "error" ? (
+          <p>Error fetching products</p>
+        ) : status === "loading" ? (
+          <p>Fetching products</p>
+        ) : (
+          <ProductSlider products={otherProducts} />
+        )}
+      </div>
     </div>
   );
 }
