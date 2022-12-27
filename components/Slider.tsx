bf1bec97498c90b097d7d513093010bc04cc19a4
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 13:22:23 +0100

    categories updated

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -12,10 +12,10 @@ export default function Slider() {
           <SplideSlide key={item.img}>
             <div className="content p-10 flex flex-col mx-auto justify-center lg:p-20">
               <h6 className="text-lg font-medium">{item.text1}</h6>
-              <h1 className="lg:text-3xl text-2xl lg:w-1/3 leading-snug font-bold my-4 lg:my-8">
+              <h1 className="lg:text-3xl w-full text-2xl lg:w-1/3 leading-snug font-bold my-4 lg:my-8">
                 {item.main}
               </h1>
-              <p className="text-lg lg:w-1/2">{item.text2}</p>
+              <p className="lg:text-lg lg:w-1/2">{item.text2}</p>
               <Button
                 text="Shop Now"
                 className="rounded-md h-10 bg-white w-32 mt-10"
