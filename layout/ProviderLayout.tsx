9a8c2e462e4a1fdf661661dc24215bda14edb2d8
BisiDavi <oludavidconnect@gmail.com>
2022-12-27 22:02:44 +0100

    nprogress added

--- layout/ProviderLayout.tsx
+++ layout/ProviderLayout.tsx
@@ -1,12 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import dynamic from "next/dynamic";
 import { PropsWithChildren } from "react";
+
+const NextNProgress = dynamic(
+  () => import(/* webpackChunkName: 'NProgress' */ "@/components/NProgress")
+);
 
 export default function ProviderLayout({ children }: PropsWithChildren) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <NextNProgress
+        color="#f54c4c"
+        height={5}
+        options={{ showSpinner: true }}
+      />
       {children}
       <ReactQueryDevtools />
     </QueryClientProvider>
