975ffbbd6cf1e626fdf132dde85775f076e70eca
BisiDavi <oludavidconnect@gmail.com>
2023-01-04 14:38:38 +0100

    FilterCategory added

---
+++ components/FilterCategory.tsx
@@ -0,0 +1,60 @@
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+  categoryData: { data: string[] } | undefined;
+  categoryStatus: "success" | "loading" | "error";
+  selectedCategory: string;
+  setSelectedCategory: Dispatch<SetStateAction<string>>;
+}
+
+export default function FilterCategory({
+  categoryData,
+  categoryStatus,
+  selectedCategory,
+  setSelectedCategory,
+}: Props) {
+  function selectCateory(category: string) {
+    setSelectedCategory(category);
+  }
+
+  const activeHome =
+    selectedCategory === "?limit=100" ? "bg-blue-900 text-white" : "";
+
+  return (
+    <div className="filters p-4 w-1/5 rounded border ">
+      <h3 className="font-bold text-xl">Filter By Category</h3>
+      <ul>
+        {categoryStatus === "error" ? (
+          <p>unable to fetch category</p>
+        ) : categoryStatus === "loading" ? (
+          <p>fetching categories ...</p>
+        ) : (
+          <>
+            <li
+              className={`${activeHome} my-2 rounded border fit-content px-4 py-1 hover:bg-gray-800 hover:text-white`}
+              onClick={() => selectCateory("?limit=100")}
+            >
+              All
+            </li>
+            {categoryData &&
+              categoryData.data.map((item) => {
+                const activeCategory =
+                  selectedCategory === `/category/${item}`
+                    ? "bg-blue-900 text-white"
+                    : "";
+                return (
+                  <li
+                    key={item}
+                    className={`${activeCategory} my-2 rounded border fit-content px-4 py-1 hover:bg-gray-800 hover:text-white`}
+                    onClick={() => selectCateory(`/category/${item}`)}
+                  >
+                    {item}
+                  </li>
+                );
+              })}
+          </>
+        )}
+      </ul>
+    </div>
+  );
+}
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 import TopProductGrid from "@/components/TopProductGrid";
 import Layout from "@/layout";
 import BreadCrumb from "@/components/BreadCrumb";
 import { fetchCategories, fetchProducts } from "@/utils/apiRequest";
 import ProductLoader from "@/components/ProductLoader";
-import { useState } from "react";
+import FilterCategory from "@/components/FilterCategory";
 
 export default function ProductsPage() {
   const [selectedCategory, setSelectedCategory] = useState("?limit=100");
@@ -21,12 +22,6 @@ export default function ProductsPage() {
     fetchCategories
   );
 
-  console.log("selectedCategory", selectedCategory);
-
-  function selectCateory(category: string) {
-    setSelectedCategory(category);
-  }
-
   return (
     <Layout title="Buy our products at a discounted price">
       <section className="container mx-auto my-4 px-4 lg:px-0">
@@ -35,34 +30,12 @@ export default function ProductsPage() {
         />
         <h2 className="text-center text-2xl font-bold">All Products</h2>
         <div className="content space-x-4 px-0 flex items-start pt-8 flex-col lg:flex-row justify-between">
-          <div className="filters p-4 w-1/5 rounded border ">
-            <h3 className="font-bold text-xl">Filter By Category</h3>
-            <ul>
-              {categoryStatus === "error" ? (
-                <p>unable to fetch category</p>
-              ) : categoryStatus === "loading" ? (
-                <p>fetching categories</p>
-              ) : (
-                <>
-                  <li
-                    className="my-2 rounded border fit-content px-4 py-1 hover:bg-gray-800 hover:text-white"
-                    onClick={() => selectCateory("?limit=100")}
-                  >
-                    All
-                  </li>
-                  {categoryData.data.map((item) => (
-                    <li
-                      key={item}
-                      className="my-2 rounded border fit-content px-4 py-1 hover:bg-gray-800 hover:text-white"
-                      onClick={() => selectCateory(`/category/${item}`)}
-                    >
-                      {item}
-                    </li>
-                  ))}
-                </>
-              )}
-            </ul>
-          </div>
+          <FilterCategory
+            categoryData={categoryData}
+            categoryStatus={categoryStatus}
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
           <div className="products w-full px-0 lg:w-3/4">
             {status === "error" ? (
               <p>Unable to fetch products</p>
