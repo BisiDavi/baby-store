39775d3a1ff4c8a55cdcafbcf9684d1175aac836
BisiDavi <oludavidconnect@gmail.com>
2023-01-05 01:22:30 +0100

    blog page updated

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -5,6 +5,7 @@ import blogContent from "@/json/blogs.json";
 import Layout from "@/layout";
 import toSlug from "@/utils/toSlug";
 import User from "@/public/icon/User";
+import BreadCrumb from "@/components/BreadCrumb";
 
 interface Props {
   blog: {
@@ -19,16 +20,25 @@ interface Props {
 export default function Blog({ blog }: Props) {
   return (
     <Layout title={`${blog.title}`}>
-      <section className="container mx-auto flex py-6 space-x-4">
-        <div className="w-1/2">
-          <Image src={blog.img} alt={blog.title} height={1000} width={1000} />
-        </div>
-        <div className="w-1/2">
-          <h4 className="text-3xl font-bold mb-2">{blog.title}</h4>
-          <p className="leading-relaxed">{blog.text}</p>
-          <div className="author mt-4 items-center flex w-1/3 font-medium">
-            <span className="mr-2">Author: </span>{" "}
-            <User className="w-1/12 mr-2" /> {blog.author}
+      <section className="container mx-auto flex flex-col py-6">
+        <BreadCrumb
+          links={[
+            { name: "🏠 Home", link: "/" },
+            { name: "Blogs", link: "/blogs" },
+            { name: blog.title },
+          ]}
+        />
+        <div className="content w-full flex space-x-6 mt-4">
+          <div className="w-1/2">
+            <Image src={blog.img} alt={blog.title} height={1000} width={1000} />
+          </div>
+          <div className="w-1/2">
+            <h4 className="text-3xl font-bold mb-2">{blog.title}</h4>
+            <p className="leading-relaxed">{blog.text}</p>
+            <div className="author mt-4 items-center flex w-1/3 font-medium">
+              <span className="mr-2">Author: </span>{" "}
+              <User className="w-1/12 mr-2" /> {blog.author}
+            </div>
           </div>
         </div>
       </section>
