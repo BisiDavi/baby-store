73fbb39fe2ba35b9e49a33cd9e576d9b44eec1b9
BisiDavi <oludavidconnect@gmail.com>
2023-01-12 13:57:49 +0100

    checkout updated

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,14 +1,22 @@
 import Layout from "@/layout";
 import checkoutForm from "@/json/checkout.json";
 import { displayForm } from "@/components/form";
+import useCart from "@/hooks/useCart";
+import SlideCartItem from "@/components/SlideCartItem";
+import useCartMutation from "@/hooks/useCartMutation";
 
 export default function Checkout() {
+  const { cart } = useCart();
+  const { useDeleteProductFromCart, useUpdateProductQuantityMutation } =
+    useCartMutation();
+  const mutateDelete = useDeleteProductFromCart();
+  const mutateUpdateQuantity = useUpdateProductQuantityMutation();
   return (
     <Layout title="Checkout">
       <section className="container py-4 flex mx-auto space-x-6">
         <div className="w-2/3">
           <h4 className="font-bold text-gray-500">CHECKOUT</h4>
-          <div className="bg-white w-full rounded py-6">
+          <div className="bg-white shadow w-full rounded py-6 my-1">
             <div className="form-title border-b w-full px-4">
               1. ADDRESS DETAILS
             </div>
@@ -32,6 +40,16 @@ export default function Checkout() {
         </div>
         <div className="w-1/3">
           <h4 className="font-bold text-gray-500">ORDER SUMMARY</h4>
+          <div className="order-summary shadow my-1 p-1 bg-white rounded">
+            {cart?.items.map((item) => (
+              <SlideCartItem
+                key={item.id}
+                item={item}
+                mutateUpdateQuantity={mutateUpdateQuantity}
+                mutateDelete={mutateDelete}
+              />
+            ))}
+          </div>
         </div>
       </section>
     </Layout>
