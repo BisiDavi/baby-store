3026715c5e4ff026f517915c47c7d2b2dc14378c
BisiDavi <oludavidconnect@gmail.com>
2023-01-12 15:10:25 +0100

    CheckoutForm added

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,36 +1,12 @@
 import Layout from "@/layout";
-import { displayForm } from "@/components/form";
 import OrderSummary from "@/components/OrderSummary";
-import checkoutForm from "@/json/checkout.json";
+import CheckoutForm from "@/components/form/CheckoutForm";
 
 export default function Checkout() {
   return (
     <Layout title="Checkout">
       <section className="container py-4 flex mx-auto space-x-6">
-        <div className="w-2/3">
-          <h4 className="font-bold text-gray-500">CHECKOUT</h4>
-          <div className="bg-white shadow w-full rounded py-6 my-1">
-            <div className="form-title border-b w-full px-4">
-              1. ADDRESS DETAILS
-            </div>
-            <div className="form-content p-4">
-              {checkoutForm.map((item, index) => {
-                return (
-                  <div
-                    className="input-row flex space-x-4 justify-between"
-                    key={index}
-                  >
-                    {item.map((inputItem) => (
-                      <div className="form-wrapper w-full" key={inputItem.name}>
-                        {displayForm(inputItem)}
-                      </div>
-                    ))}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
+        <CheckoutForm />
         <OrderSummary />
       </section>
     </Layout>
