26ba25d0154b422f3da8ba98d1db46c20b14aeac
BisiDavi <oludavidconnect@gmail.com>
2023-01-15 16:01:52 +0100

    stripe server added

---
+++ pages/api/stripe-server.ts
@@ -0,0 +1,20 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req: any, res: any) {
+  switch (req.method) {
+    case "POST": {
+      try {
+        const session = await stripe.checkout.sessions.create({
+          line_items: req.body.lineItems,
+          mode: "payment",
+          success_url: `${req.headers.origin}/checkout?status=success`,
+          cancel_url: `${req.headers.origin}/checkout?status=cancelled`,
+        });
+        res.redirect(303, session.url);
+      } catch (error: any) {
+        res.status(error.statusCode || 500).json(error.message);
+      }
+    }
+  }
+  res.status(200).json({ name: "John Doe" });
+}
