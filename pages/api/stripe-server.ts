958a36e3fc07d3462ef0097aea179c08b9f962df
BisiDavi <oludavidconnect@gmail.com>
2023-01-16 04:16:02 +0100

    stripe server updated

--- pages/api/stripe-server.ts
+++ pages/api/stripe-server.ts
@@ -7,13 +7,19 @@ export default async function handler(req: any, res: any) {
         const session = await stripe.checkout.sessions.create({
           line_items: req.body.lineItems,
           mode: "payment",
-          success_url: `${req.headers.origin}/checkout?status=success`,
-          cancel_url: `${req.headers.origin}/checkout?status=cancelled`,
+          success_url: `${req.headers.origin}/checkout/success?session_id=CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/checkout/error`,
         });
+        console.log("session", session);
         res.redirect(303, session.url);
+        // res.status(200).json({ session });
       } catch (error: any) {
         res.status(error.statusCode || 500).json(error.message);
       }
+    }
+    default: {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method Not Allowed");
     }
   }
 }
