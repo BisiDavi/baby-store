88a9efa753e24140cdd16925b565f740d50cc950
BisiDavi <oludavidconnect@gmail.com>
2023-01-16 13:56:20 +0100

    currency-rate added

--- pages/api/currency-rate.ts
+++ pages/api/currency-rate.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,15 +10,37 @@ export default async function handler(
   switch (req.method) {
     case "GET": {
       try {
-        const currencyRequest = await axios.get(
-          `${process.env.APILAYER_BASE_URL}/live?source=USD&currencies=EUR%2CNGN%2CUSD%2CCAD`,
-          {
-            headers: {
-              apikey: process.env.APILAYER_CURRENCY_KEY,
-            },
-          }
+        const filePath = path.join(
+          __dirname,
+          "..",
+          "..",
+          "..",
+          "..",
+          "json",
+          "currency-rate.json"
         );
-        res.status(200).json({ result: currencyRequest.data });
+        await axios
+          .get(
+            `${process.env.APILAYER_BASE_URL}/live?source=USD&currencies=EUR%2CNGN%2CUSD%2CCAD`,
+            {
+              headers: {
+                apikey: process.env.APILAYER_CURRENCY_KEY,
+              },
+            }
+          )
+          .then((response) => {
+            const currentDate = new Date();
+            const result = {
+              ...response.data,
+              date: currentDate.toLocaleDateString("en-CA"),
+            };
+            fs.writeFile(filePath, JSON.stringify(result), (err) => {
+              if (err) {
+                return res.status(400).json({ status: err });
+              }
+              return res.status(200).json({ status: "currency-rate updated" });
+            });
+          });
       } catch (error: any) {
         res.status(error.statusCode || 500).json(error.message);
       }
--- utils/getCurrencyRate.ts
+++ utils/getCurrencyRate.ts
@@ -1,5 +1,12 @@
+import currencyRate from "@/json/currency-rate.json";
+import axios from "axios";
+
 export default function getCurrencyRate() {
-  return (
-    <div>getCurrencyRate</div>
-  )
+  const currentDateInstance = new Date().toLocaleDateString("en-CA");
+  const currentDateTime = new Date(currentDateInstance).getTime();
+  const currencyDateTime = new Date(currencyRate.date).getTime();
+
+  if (currentDateTime > currencyDateTime) {
+    axios.get("/api/currency-rate");
+  }
 }
