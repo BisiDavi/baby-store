67da39f7bea73a1153eae02dc424cd0a51b8f539
BisiDavi <oludavidconnect@gmail.com>
2023-01-16 14:59:56 +0100

    cart updated

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -1,10 +1,11 @@
 import useCart from "@/hooks/useCart";
-import { useAppSelector } from "@/redux/store";
+import useStripePayment from "@/hooks/useStripePayment";
 import useCartMutation from "@/hooks/useCartMutation";
 import SlideCartItem from "@/components/SlideCartItem";
-import { formatPrice } from "@/utils/formatPrice";
 import Button from "@/components/Button";
-import useStripePayment from "@/hooks/useStripePayment";
+import { formatPrice } from "@/utils/formatPrice";
+import { useAppSelector } from "@/redux/store";
+import usePrice from "@/hooks/usePrice";
 
 export default function OrderSummary() {
   const { cart } = useCart();
@@ -14,9 +15,11 @@ export default function OrderSummary() {
   const { checkoutDetails } = useAppSelector((state) => state.checkout);
   const mutateDelete = useDeleteProductFromCart();
   const mutateUpdateQuantity = useUpdateProductQuantityMutation();
-  const DELIVERY_FEE = 80;
-  const total = cart ? DELIVERY_FEE + cart.amount : 0;
+  const { rate, currency } = usePrice();
+  const DELIVERY_FEE = 80 * rate;
   const disableButton = !checkoutDetails ? true : false;
+  const itemAmount = cart ? cart.amount * rate : 0;
+  const total = cart ? DELIVERY_FEE + itemAmount : 0;
 
   return (
     <div className="w-1/3">
@@ -37,17 +40,26 @@ export default function OrderSummary() {
             <div className="total mt-4 px-4">
               <div className="row my-1 flex items-center justify-between">
                 <h4 className="font-bold">Subtotal</h4>
-                <h4>${formatPrice(cart.amount)}</h4>
+                <h4>
+                  {currency.value}
+                  {formatPrice(itemAmount)}
+                </h4>
               </div>
               {cart.amount < 1000 && (
                 <div className="row my-1 flex items-center justify-between">
                   <h4 className="font-bold">Delivery Fee</h4>
-                  <h4>${formatPrice(DELIVERY_FEE)}</h4>
+                  <h4>
+                    {currency.value}
+                    {formatPrice(DELIVERY_FEE)}
+                  </h4>
                 </div>
               )}
               <div className="border-t row flex items-center justify-between py-2">
                 <h4 className="font-bold">Total</h4>
-                <h4 className="font-bold">${formatPrice(total)}</h4>
+                <h4 className="font-bold">
+                  {currency.value}
+                  {formatPrice(total)}
+                </h4>
               </div>
             </div>
             <Button
