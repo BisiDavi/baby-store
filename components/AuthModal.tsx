977d55dbcb0f0005972853ebe0386f6696c0cdc8
BisiDavi <oludavidconnect@gmail.com>
2023-01-16 17:23:36 +0100

    auth modal updated

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -6,10 +6,19 @@ export default function AuthModal() {
   const { authModalHandler, authModal } = useUI();
 
   return (
-    <Modal modal={authModal} modalHandler={authModalHandler}>
-      <div className="authmodal">
-        <h4>Please Login to Proceed</h4>
-        <Button className="" text="Login with Google" />
+    <Modal
+      modal={authModal}
+      modalHandler={authModalHandler}
+      modalHeaderClassName="w-96"
+    >
+      <div className="authmodal w-full">
+        <h4 className="font-bold text-2xl flex mx-auto items-center  justify-center">
+          Please Login to Proceed
+        </h4>
+        <Button
+          className="my-10 mx-auto justify-center flex border px-4 rounded py-1 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white"
+          text="  Login with Google"
+        />
       </div>
     </Modal>
   );
