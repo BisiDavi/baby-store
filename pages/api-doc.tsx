c5f5ec0f3c4ed85087fac04ff17a0ea7490c1f77
BisiDavi <oludavidconnect@gmail.com>
2023-01-23 00:55:41 +0100

    api-docs updated

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -17,7 +17,7 @@ export const getStaticProps: GetStaticProps = async () => {
     definition: {
       openapi: "3.0.0",
       info: {
-        title: "Next Swagger API Example",
+        title: "BestStore API Docs",
         version: "1.0",
       },
     },
